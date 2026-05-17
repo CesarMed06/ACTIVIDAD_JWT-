24916789a8b79ba7f78167a9cf63b0e586bf4f64
CesarMed06 <cmedsag@alumnos.cei.es>
2026-05-17 20:51:37 +0200

    Corregir mayuscula en require de jugador

--- routes/jugador.js
+++ routes/jugador.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Jugador = require('../models/Jugador');
+const Jugador = require('../models/jugador');
 
 router.post('/', async (req, res) => {
     try {
